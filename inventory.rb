4423d00b742741c025cd6d1a1e4d324434eaa849
Dustin Keppler <dtkeppler@yahoo.com>
2015-08-09 12:59:46 -0400

    added delete method

--- inventory.rb
+++ inventory.rb
@@ -22,4 +22,12 @@ if user_action == "add"
 	beer_count[add_beer] = add_qty
 	puts "#{add_qty} cases of #{add_beer} have been added to inventory"
 	puts beer_count
+elsif user_action == "delete"
+	puts "Which beer would you like to delete from inventory?"
+	user_delete = gets.chomp
+	beer_count.delete(user_delete)
+	puts "#{user_delete} has been removed from inventory!"
+	puts beer_count
+else
+	puts "You've entered an invalid command"
 end
